3d5cba035383647f84a1b3046819493a3eb33f02
unknown <opusidea@gmail.com>
2025-12-05 12:30:29 +0100

    j3 lunch

--- demo-router/src/components/articles/index.vue
+++ demo-router/src/components/articles/index.vue
@@ -1,7 +1,24 @@
+<script setup>
+import axios from 'axios'
+import { reactive, onMounted } from 'vue'
+
+let state = reactive({
+    articles: []
+})
+
+onMounted(async () => {
+
+    const res = await axios.get('http://localhost:3000/articles');
+    state.articles = res.data; // state update
+
+})
+
+</script>
+
 <template>
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 
-        <div class="col">
+        <div class="col" v-for="article in state.articles">
             article
         </div>
 
